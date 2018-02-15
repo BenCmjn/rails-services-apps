33a3ec1b592bf124c9f62fce3890178853e5c21f
BenCmjn <benjamin.calmejane@gmail.com>
2018-02-15 14:45:00 +0100

    creation 2e app : twitter-bis

--- rails-service/app/services/say_hello.rb
+++ rails-service/app/services/say_hello.rb
@@ -1,3 +1,12 @@
+# Appelle la gem dotenv
+require 'dotenv'
+require 'twitter'
+
+# Ceci appelle le fichier .env grâce à la gem dotenv, et enregistre toutes les données enregistrées dans une hash ENV
+Dotenv.load
+# Il est très facile d'appeler les données sensibles du fichier .env, par exemple là je vais afficher TWITTER_API_SECRET
+puts ENV['TWITTER_API_SECRET']
+
 class SayHello
   def perform
     p "bonjour"
